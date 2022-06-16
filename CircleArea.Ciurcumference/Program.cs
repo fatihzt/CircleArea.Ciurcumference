c16fc90ad7c92e98c8b56b47e5223adaaa58fd98
fatihzt <muhammetfatihozata7@gmail.com>
2022-06-16 11:47:08 +0300

    Add project files.

---
+++ CircleArea.Ciurcumference/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+Console.WriteLine("-----------------------------------------");
+Console.WriteLine("Calculating Circle Area and Circumference");
+Console.WriteLine("-----------------------------------------");
+
+Console.WriteLine("Please enter a radius:");
+var value = Console.ReadLine();
+if (string.IsNullOrEmpty(value))
+{
+    Console.WriteLine("Please enter a integer number.");
+    return;
+}
+if (int.TryParse(value, out int x))
+{
+    Console.WriteLine(" ");
+}
+else
+{
+    Console.WriteLine("Please!!!1 enter a integer number ");
+}
+double radius = Convert.ToDouble(value);
+double area = Math.PI * radius * radius;
+Console.WriteLine("area is " + area);
+
+double circum = 2 * Math.PI * radius;
+Console.WriteLine("circumference is " + circum);
